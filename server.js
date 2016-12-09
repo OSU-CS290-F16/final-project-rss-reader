7b451927c247819f27437dfd9ef37f84d043d705
Zach Gangwer <zach.gangwer@gmail.com>
2016-12-08 20:34:22 -0800

    Abandoning Branch, Focus Changed

--- server.js
+++ server.js
@@ -1,46 +1,63 @@
+// Native Dependencies
+var path = require("path");
+
+// External Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
-var mongo = require("mongodb").MongoClient;
-var assert = require("assert");
 
-var url = "mongodb://localhost:27017/rss";
-var app = express();
+// Local Dependencies
+var mongo = require("./mongo-wrap.js");
+var rss = require("rss-feeder");
+
+// Global Variables
 var port = process.env.PORT || 3000;
+var app = express();
 
-// Use Handlebars as the view engine for the app.
+// Use Handlebars as the View Engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Serve static files from PUBLIC
+app.use(express.static(path.join(__dirname, "public")));
 
-// Set static serve folder
-app.use(express.static("public"));
-
-mongo.connect(url, function(err, db) {
-	assert.equal(null, err);
-	// console.log("Connected to MongoDB");
-
-	db.close();
-});
-
+/*
+ * Route the root path ('/') to the index page.  Give Handlebars the
+ * appropriate page title and data for all users to render.
+ */
 app.get("/", function (req, res) {
-	res.render("index");
+	res.render("index-page", {
+		title: "Readdit",
+		usersData: mongo.query("user")
+	});
 });
 
-// app.get("/notes/:user", function(req, res){
-// 	var user = usersData[req.params.user];
-//
-// 	if (user){
-// 		res.render("notes-page", user);
-// 	} else {
-// 		res.render("404-page", {status: 404});
-// 	}
-// });
+/*
+ * Set up a dynamic route to render a page with a specified user's rss feed
+ * Pulls the users feedList array and parses each feed, creating a list.
+ */
+app.get("/feed/:user", function (req, res, next) {
+	var userData = mongo.query(req.params.user);
 
+	if (userData) {
+		res.render("feed-page", {
+			title: "Readdit - " + userData.name,
+			articles: rss.parse(userData.subscriptions),
+			userName: userData.name
+		});
+	} else {
+		next();
+	}
+
+});
+
+// Return a 404 and render the 404 page for any other route.
 app.get("*", function (req, res) {
-	res.render("404-page", {status: 404});
+	res.status(404).render("404-page", {
+		title: "ToDoIt"
+	});
 });
 
 // Listen on the specified port.
 app.listen(port, function () {
-	//console.log("== Listening on port: ", port);
+	//console.log("== Listening on port", port);
 });
